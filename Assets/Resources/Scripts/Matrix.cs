60a25a04fa087bdb94ca8b14468d349b1b8181e6
AllFatherGray <denziegray@outlook.com>
2017-01-08 03:19:55 -0500

    Added some scripts to start off the git. A state machine for menus in Unity3d is next

---
+++ Assets/Resources/Scripts/Matrix.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+
+
+public enum MatrixConstraints : byte { NONE = 0, ROW = 2, COL = 4, ROWCOL = 6, HEIGHT = 8, HROW = 10, HCOL = 12, ALL = 14 }
+/// <summary>
+/// 3 dimensional array that uses a single dimensional array underneath
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Matrix<T> : IEnumerable
+{
+    uint row, col, height;
+    T[] matrixArray;
+    public Matrix(uint row, uint col, uint height)
+    {
+        this.row = row;
+        this.col = col;
+        this.height = height;
+        matrixArray = new T[Length];
+    }
+    public Matrix(int row, int col, int height)
+    {
+        this.row = Convert.ToUInt32(row);
+        this.col = Convert.ToUInt32(col);
+        this.height = Convert.ToUInt32(height);
+        matrixArray = new T[Length];
+    }
+    public uint Length
+    {
+        get
+        {
+            return row * col * height;
+        }
+    }
+    public uint Height
+    {
+        get
+        {
+            return height;
+        }
+    }
+    public uint Column
+    {
+        get
+        {
+            return col;
+        }
+    }
+    public uint Row
+    {
+        get
+        {
+            return row;
+        }
+    }
+    public T this[int key1, int key2, int key3 = 0]
+    {
+        get
+        { return matrixArray[((col * key1) + key2) + (key3 * row * col)]; }
+        set
+        { matrixArray[((col * key1) + key2) + (key3 * row * col)] = value; }
+    }
+    public T this[uint key1, uint key2, uint key3 = 0]
+    {
+        get
+        { return matrixArray[((col * key1) + key2) + (key3 * row * col)]; }
+        set
+        { matrixArray[((col * key1) + key2) + (key3 * row * col)] = value; }
+    }
+
+    public T this[uint key]
+    {
+        get
+        {
+            return matrixArray[key];
+        }
+        set
+        {
+            matrixArray[key] = value;
+        }
+    }
+    public T this[int key]
+    {
+        get
+        {
+            return matrixArray[key];
+        }
+        set
+        {
+            matrixArray[key] = value;
+        }
+    }
+    public IEnumerator GetEnumerator()
+    {
+        return matrixArray.GetEnumerator();
+    }
+    public static implicit operator bool(Matrix<T> m)
+    {
+        return m as object != null;
+    }
+}
---
+++ Assets/Resources/Scripts/vFuncWrapper.cs
@@ -0,0 +1,71 @@
+﻿
+//// <summary>
+/// Delegate Wrapper for T  where T is the Nougat-y goodness - Allows null-default param arguments for structs
+/// </summary>
+public class vFuncWrapper<T>
+{
+    /// <summary>
+    /// The shell that contains the stored T - yum
+    /// </summary>
+    public delegate T vFunc();
+    /// <summary>
+    /// Instance of candy shell
+    /// </summary>
+    vFunc _shell = null;
+    public vFuncWrapper(T _candy = default(T)) { _shell = new vFunc(delegate () { return _candy; }); }
+    public vFuncWrapper(vFuncWrapper<T> vfw) : this(vfw._shell()) { }
+    public static implicit operator vFuncWrapper<T>(T _shell)
+    {
+        return new vFuncWrapper<T>(_shell);
+    }
+    public static implicit operator vFunc(vFuncWrapper<T> vfw)
+    {
+        return vfw ? vfw._shell : null;
+    }
+    public static implicit operator vFuncWrapper<T>(vFunc vf)
+    {
+        return vf != null ? new vFuncWrapper<T>(vf) : null;
+    }
+    public static implicit operator bool(vFuncWrapper<T> vfw)
+    {
+        return (vfw as object) != null && vfw._shell != null;
+    }
+    public static implicit operator T(vFuncWrapper<T> vfw)
+    {
+        try
+        {
+            return vfw._shell();
+        }
+        catch
+        {
+            return default(T);
+        }
+    }
+
+}
+/// <summary>
+/// Extension Class for vFuncWrapper. Allows Objects to Wrap themselves
+/// </summary>
+public static class vFuncWrapper
+{
+    /// <summary>
+    /// Wraps any object / primitive into a delagate based wrapper
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="candy">The object to wrap</param>
+    /// <returns></returns>
+    public static vFuncWrapper<T> Create<T>(T candy)
+    {
+        return new vFuncWrapper<T>(candy);
+    }
+    /// <summary>
+    /// Extension Method that Wraps any object / primitive into a delagate based wrapper
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="candy">The object to wrap</param>
+    /// <returns></returns>
+    public static vFuncWrapper<T> vWrap<T>(this T candy)
+    {
+        return new vFuncWrapper<T>(candy);
+    }
+}

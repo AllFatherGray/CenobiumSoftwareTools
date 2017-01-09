e30d6ec1b74cbaf019501f4d4da013c871333d60
AllFatherGray <denziegray@outlook.com>
2017-01-09 00:21:42 -0500

    Removed extra or unnecessary comments and sdded more comments explaining the use of vFuncWrappers methods

--- Assets/Resources/Scripts/vFuncWrapper.cs
+++ Assets/Resources/Scripts/vFuncWrapper.cs
@@ -1,38 +1,73 @@
 ﻿namespace Cenobium
 {
     //// <summary>
-    /// Delegate Wrapper for T  where T is the Nougat-y goodness - Allows null-default param arguments for structs
+    /// Generic Wrapper for objects  - Allows null-default param arguments for structs
     /// It is similar to the null-able structs you get in C# but with 
-    /// that added bonus of being a class and having an implicit cast to a delegate that also return the wrapped type.
+    /// that added bonus of being a class and having an implicit cast to a delegate that also returns the wrapped type.
     /// </summary>
     public class vFuncWrapper<T>
     {
         /// <summary>
-        /// The shell that contains the stored T - yum
+        /// The delegate that contains the stored object
         /// </summary>
         public delegate T vFunc();
         /// <summary>
-        /// Instance of candy shell
+        /// Instance of vFunc shell to be used by an instance of the class
         /// </summary>
         vFunc _shell = null;
+        /// <summary>
+        /// Default constructor. Wraps the object passed to method. Here , _shell is assigned a delegate that returns the parameter arg passed to the constructor
+        /// </summary>
+        /// <param name="_candy"></param>
         public vFuncWrapper(T _candy = default(T)) { _shell = new vFunc(delegate () { return _candy; }); }
+        /// <summary>
+        /// Copy Constructor
+        /// </summary>
+        /// <param name="vfw"></param>
         public vFuncWrapper(vFuncWrapper<T> vfw) : this(vfw._shell()) { }
+
+        /// <summary>
+        /// The _shell is wrapped in a new vFuncWrapper object - similar to default constructor
+        /// </summary>
+        /// <param name="_shell"></param>
+        /// <returns></returns>
         public static implicit operator vFuncWrapper<T>(T _shell)
         {
             return new vFuncWrapper<T>(_shell);
         }
+        
+        /// <summary>
+        /// Wrapper returns the vFunc delegate
+        /// </summary>
+        /// <param name="vfw"></param>
+        /// <returns></returns>
         public static implicit operator vFunc(vFuncWrapper<T> vfw)
         {
             return vfw ? vfw._shell : null;
         }
+        /// <summary>
+        /// Converts a delegate into a vFuncWrapper
+        /// </summary>
+        /// <param name="vf"></param>
+        /// <returns></returns>
         public static implicit operator vFuncWrapper<T>(vFunc vf)
         {
             return vf != null ? new vFuncWrapper<T>(vf) : null;
         }
+        /// <summary>
+        /// Returns true if object isn't null
+        /// </summary>
+        /// <param name="vfw"></param>
+        /// <returns></returns>
         public static implicit operator bool(vFuncWrapper<T> vfw)
         {
-            return (vfw as object) != null && vfw._shell != null;
+            return vfw != null && vfw._shell != null;
         }
+        /// <summary>
+        /// Returns the wrapped object - similar to GetValueOrDefault
+        /// </summary>
+        /// <param name="vfw"></param>
+        /// <returns></returns>
         public static implicit operator T(vFuncWrapper<T> vfw)
         {
             try
@@ -57,7 +92,7 @@
         /// <typeparam name="T"></typeparam>
         /// <param name="candy">The object to wrap</param>
         /// <returns></returns>
-        public static vFuncWrapper<T> Create<T>(T candy)
+        public static vFuncWrapper<T> vCreate<T>(T candy)
         {
             return new vFuncWrapper<T>(candy);
         }
